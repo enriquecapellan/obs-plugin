800513a1f86557a9b953beca4ac00ab3ce38225c
Enrique Capellan <enriqcapellan@gmail.com>
2022-05-01 08:54:58 -0400

    make some style changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import '../assets/style.css'
+import '../assets/panel.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- pages/socket.tsx
+++ pages/socket.tsx
@@ -21,17 +21,19 @@ const Home = () => {
 	};
 
 	return (
-		<Wrapper>
+		<div className="panel">
 			<div className="container2">
-				<div className="checkbox-container">
-					<label htmlFor="instagram">Instagram</label>
-					<input
-						type="checkbox"
-						id="instagram"
-						onChange={(e) => {
-							socket && socket.emit("set-instagram", e.target.checked);
-						}}
-					/>
+				<div className="setting-container">
+					<div className="checkbox-container">
+						<label htmlFor="instagram">Instagram</label>
+						<input
+							type="checkbox"
+							id="instagram"
+							onChange={(e) => {
+								socket && socket.emit("set-instagram", e.target.checked);
+							}}
+						/>
+					</div>
 				</div>
 
 				<div className="setting-container">
@@ -53,50 +55,8 @@ const Home = () => {
 					</div>
 				</div>
 			</div>
-		</Wrapper>
+		</div>
 	);
 };
-
-const Wrapper = styled.div`
-	font-family: helvetica;
-	font-size: 1.2rem;
-
-	.contiainer2 {
-		display: flex;
-		flex-direction: column;
-	}
-
-	.setting-container {
-		margin-top: 1rem;
-		border-radius: 8px;
-		border: 1px solid gray;
-		padding: 1rem;
-		display: flex;
-		flex-direction: column-reverse;
-		gap: 0.5rem;
-
-		input {
-			border-radius: 4px;
-			border: 1px solid gray;
-			height: 1.2rem;
-			padding: 0.3rem;
-		}
-
-		background-color: #304c72;
-		color: white;
-	}
-
-	input[type="checkbox"] {
-		margin: 0;
-	}
-
-	.checkbox-container {
-		display: flex;
-		flex-direction: row-reverse;
-		justify-content: flex-end;
-		align-items: center;
-		gap: 0.2rem;
-	}
-`;
 
 export default Home;
