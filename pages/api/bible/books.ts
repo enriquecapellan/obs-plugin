c0917a2c1ccedca2cda4f186b4b5ac937f870aba
Enrique Capellan <enriqcapellan@gmail.com>
2023-05-07 08:18:59 -0400

    get bible from new api

--- pages/api/bible/books.ts
+++ pages/api/bible/books.ts
@@ -1,7 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	fetch("https://bible-app-demo.herokuapp.com/api/books/")
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	fetch(`${process.env.API}/books/`)
 		.then((res) => res.json())
 		.then((response) => res.status(200).json(response));
 }
--- pages/api/bible/verses/[...slug].ts
+++ pages/api/bible/verses/[...slug].ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
 	const { slug } = req.query;
 
-	fetch(`https://bible-app-demo.herokuapp.com/api/books/${slug[0]}/verses/${slug[1]}`)
+	fetch(`${process.env.API}/books/${slug[0]}/verses/${slug[1]}`)
 		.then((res) => res.json())
 		.then((response) => res.status(200).json(response));
 }
